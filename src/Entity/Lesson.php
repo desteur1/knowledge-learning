bb5fb343befcdcf0f4f893fc96cfcd13108c8438
Desteur Fall <shmurdanoah@gmail.come>
2026-04-16 19:35:06 +0200

    themes, cursus and lesson entities created

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
+class Lesson
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $videoUrl = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Cursus $cursus = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getVideoUrl(): ?string
+    {
+        return $this->videoUrl;
+    }
+
+    public function setVideoUrl(string $videoUrl): static
+    {
+        $this->videoUrl = $videoUrl;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCursus(): ?Cursus
+    {
+        return $this->cursus;
+    }
+
+    public function setCursus(?Cursus $cursus): static
+    {
+        $this->cursus = $cursus;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Theme.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThemeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ThemeRepository::class)]
+class Theme
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Cursus>
+     */
+    #[ORM\OneToMany(targetEntity: Cursus::class, mappedBy: 'theme')]
+    private Collection $cursuses;
+
+    public function __construct()
+    {
+        $this->cursuses = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Cursus>
+     */
+    public function getCursuses(): Collection
+    {
+        return $this->cursuses;
+    }
+
+    public function addCursus(Cursus $cursus): static
+    {
+        if (!$this->cursuses->contains($cursus)) {
+            $this->cursuses->add($cursus);
+            $cursus->setTheme($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCursus(Cursus $cursus): static
+    {
+        if ($this->cursuses->removeElement($cursus)) {
+            // set the owning side to null (unless already changed)
+            if ($cursus->getTheme() === $this) {
+                $cursus->setTheme(null);
+            }
+        }
+
+        return $this;
+    }
+}
